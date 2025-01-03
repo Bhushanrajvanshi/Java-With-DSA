b3b66a7a390d7e45b1bccbdd7982eb1cb56a6a3e
Bhushanrajvanshi <bhushan1may@gmail.com>
2025-01-03 14:02:23 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,13 @@
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+
+// how to push code on github
+// git init
+// git add .
+// git commit -m "first commit"
+// git remote add origin https://github.com/your-username/your-repo-name.git
+// git push -u origin main
