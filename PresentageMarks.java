b8f6e201a777d5529887db251449a3895795a2e8
Bhushanrajvanshi <bhushan1may@gmail.com>
2025-01-04 19:59:16 +0530

    tenth marks

--- PresentageMarks.java
+++ PresentageMarks.java
@@ -1,11 +1,11 @@
 public class PresentageMarks {
     public static void main(String[] args) {
-        double hindi = 75;
-        double english = 80;
-        double math = 85;
-        double science = 90;
-        double social = 95;
-        double total = hindi + english + math + science + social;
+        double hindi = 47;
+        double sanskrit = 59;
+        double math = 44;
+        double science = 44;
+        double social = 49;
+        double total = hindi + sanskrit + math + science + social;
         double percentage = (total / 500) * 100;
         System.out.println("Percentage is: " + percentage + "%");
     }
