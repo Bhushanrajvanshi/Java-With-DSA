4a4edd5555ceb7eaa5740964e51b365b469e3685
Bhushanrajvanshi <bhushan1may@gmail.com>
2025-01-04 20:05:39 +0530

    board marks

--- PresentageMarks.java
+++ PresentageMarks.java
@@ -6,7 +6,8 @@ public class PresentageMarks {
         double science = 44;
         double social = 49;
         double total = hindi + sanskrit + math + science + social;
-        double percentage = (total / 500) * 100;
+        // double percentage = (total / 500) * 100;
+        double percentage = total / 5;
         System.out.println("Percentage is: " + percentage + "%");
     }
 }
