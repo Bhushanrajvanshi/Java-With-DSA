125446e7d01f1895e99d376be33566dcb95ec015
Bhushanrajvanshi <bhushan1may@gmail.com>
2025-01-13 12:50:41 +0530

    Prime number

---
+++ Loops_With_Java/PrimeNumber.java
@@ -0,0 +1,22 @@
+package Loops_With_Java;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num = sc.nextInt();
+        boolean isPrime = false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                isPrime = true;
+                break;
+            }
+        }
+        if (!isPrime) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+    }
+}
