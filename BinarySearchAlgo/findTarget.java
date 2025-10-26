7d2849c802c612030648ece505cdb92b1cde3072
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2025-10-26 07:30:50 +0530

    Find Target Value in Sorted Array by using simple way and recursion

--- BinarySearchAlgo/findTarget.java
+++ BinarySearchAlgo/findTarget.java
@@ -19,10 +19,25 @@ public class findTarget {
         }
         return ans;
     }
+
+
+    static int findTargetByRecursion(int[] arr,int start, int end, int target){
+        if( start>=end ) return -1;
+        int mid = start + (end-start)/2;
+        if (arr[mid]==target){
+            return mid;
+        } else if(arr[mid]>target){
+            return findTargetByRecursion(arr, start, mid-1, target);
+        } else {
+            return findTargetByRecursion(arr, mid+1, end, target);
+        }
+    }
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int target = 7;
+        int length = arr.length-1;
+        int target = 5;
 
         System.out.println(findTargetInSortedArray(arr, target));
+        System.out.println(findTargetByRecursion(arr, 0, length, target));
     }
 }
