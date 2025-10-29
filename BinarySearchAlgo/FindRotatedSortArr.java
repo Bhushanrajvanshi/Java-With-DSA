bdbd5eefe40c777ae808ee1c8f22117a79e0b6bb
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2025-10-29 07:27:30 +0530

    Find Target in Rotated Sort Array with Duplicate values

---
+++ BinarySearchAlgo/FindRotatedSortArr.java
@@ -0,0 +1,33 @@
+package BinarySearchAlgo;
+
+public class FindRotatedSortArr {
+    static int findValueInRotatedArray(int[] arr, int target) {
+        int start = 0, end = arr.length-1;
+        while( start<=end ){
+            int mid = start + (end-start)/2;
+            if( arr[mid]==target ) return mid;
+            if(arr[start]==arr[mid] && arr[end]==arr[mid]){
+                start++;
+                end--;
+            } else if( arr[mid]<=arr[end] ){
+                if( arr[mid]<target && arr[end]>=target ){
+                    start = mid+1;
+                } else {
+                    end = mid-1;
+                }
+            } else {
+                if( arr[mid]>target && arr[start]<=target ){
+                    end = mid-1;
+                } else {
+                    start = mid+1;
+                }
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 1, 1, 2, 2, 3, 1};
+        int target = 1;
+        System.out.println(findValueInRotatedArray(arr, target));
+    }
+}
