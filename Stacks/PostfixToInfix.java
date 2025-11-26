89455b4980bf14b3f7c62fc6ed6c50b4904ada38
Bhushan Rajvanshi <rajdevram7070@gmail.com>
2025-11-26 12:10:47 +0530

    Convert Postfix into Infix

---
+++ Stacks/PostfixToInfix.java
@@ -0,0 +1,28 @@
+package Stacks;
+
+import java.util.Stack;
+
+public class PostfixToInfix {
+    public static void main(String[] args) {
+        String postFix = "953+4*6/-";
+        Stack<String> st = new Stack<>();
+
+        for(int i=0; i<postFix.length(); i++){
+            char ch = postFix.charAt(i);
+            int ascii = (int)ch;
+
+            // ASCII Value 0 -> 48 to 9 -> 57
+            if(ascii>=48 && ascii<=57){
+                String s = "" + ch;
+                st.push(s);
+                // st.push(ascii-48);
+            } else {
+                String v1 = st.pop();
+                String v2 = st.pop();
+                String s = "(" + v2 + ch + v1 + ")";
+                st.push(s);
+            }   
+        }
+        System.out.println(st.pop());
+    }
+}
